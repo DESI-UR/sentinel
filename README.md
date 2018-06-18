337511537acff8a6095a10fb55920521893a8826
Tolga Yapici <tyapici@ur.rochester.edu>
2018-06-18 13:43:02 -0400

    initial commit of sentinel. probably it will be parsed for more features later

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sentinel
-a project to control the number of jobs to be submitted to slurm batch system
+
+This is a project to control the number of jobs to be submitted to slurm batch system.
+
+More explanation to come...
+
---
+++ sentinel.sh
@@ -0,0 +1,44 @@
+#!/bin/env bash
+
+# set these values either in your bash profile or here
+MAX_NUM_JOBS=200
+WAIT_TIME=120s
+
+QUEUE_DIRECTORY="queue"
+RUN_DIRECTORY="run"
+COMPLETED_DIRECTORY="complete"
+
+function submit_jobs() {
+    jobs_to_submit=$1
+    list_files=`ls -t ${QUEUE_DIRECTORY} | tail -n ${jobs_to_submit}` 
+    for file in ${list_files[@]}; do
+	echo "moved it to run directory"
+	mv ${QUEUE_DIRECTORY}/${file} ${RUN_DIRECTORY}/.
+	sed -i "s/queue/run/" ${RUN_DIRECTORY}/${file}
+	echo "submitting job"
+	#more ${RUN_DIRECTORY}/${file}
+	sbatch ${RUN_DIRECTORY}/${file}
+    done
+}
+
+
+function run_sentinel() {
+    while true; do
+	RUNNING_JOBS=`squeue -u ${USER} | wc -l || echo "failed"`
+	if [ "${RUNNING_JOBS}" == "failed" ]; then
+	    echo "problem with batch system..."
+	fi
+	NUM_RUNNING_JOBS=$(expr ${RUNNING_JOBS} - 1)
+	echo "Number of running johs:     " ${NUM_RUNNING_JOBS}
+	echo "Number of max jobs allowed: " ${MAX_NUM_JOBS}
+	let "SUBMIT_JOBS = ${MAX_NUM_JOBS} - ${NUM_RUNNING_JOBS}"
+	echo "Number of jobs to submit:   " ${SUBMIT_JOBS}
+	if [ ${SUBMIT_JOBS} -gt 0 ]; then
+	    submit_jobs ${SUBMIT_JOBS}
+	fi
+	echo "going to sleep"
+	sleep ${WAIT_TIME}
+    done
+}
+
+run_sentinel
